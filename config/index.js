035e3c87df8f8deb74db8729cc1a78591524a6d1
maksimvorobyov <gtl.vorobyov@gmail.com>
2018-05-12 18:28:47 +0300

    Add project

---
+++ config/index.js
@@ -0,0 +1,9 @@
+const config = {
+  // database: 'localhost/blog-lesson',
+  database: 'mongodb://127.0.0.1:27017/blog-lesson',
+
+  port: 3000,
+  secret: 'verysecretkey'
+};
+
+export default config;
---
+++ server.js
@@ -0,0 +1,40 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import session from 'express-session';
+import bodyParser from 'body-parser';
+import morgan from 'morgan';
+import bluebird from 'bluebird';
+
+import config from './config';
+import authRoute from './routes/auth';
+import errorHandler from './middlewares/errorHandler';
+
+const app = express();
+
+mongoose.Promise = bluebird;
+mongoose.connect(config.database, err => {
+  if(err){
+    throw err;
+  }
+  console.log('Mongo connected');
+
+});
+
+app.listen(config.port, err => {
+  if(err) throw err;
+
+  console.log(`Server listening on port ${config.port}`);
+});
+
+app.use(morgan('combined'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(session({
+  resave: true,
+  saveUninitialized: true,
+  secret: config.secret
+}));
+
+app.use('/api', authRoute);
+
+app.use(errorHandler);
